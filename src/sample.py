f01388894fbbf767a868ca1173f8d3c96fb065eb
rakshakonanur <raksha.konanur@berkeley.edu>
2025-07-08 14:48:44 -0700

    Sample workflow from PCA scores to simulation

---
+++ src/sample.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import pyvista as pv
+import scipy.io
+import numpy as np
+from pathlib import Path
+import os
+import json
+from argparse import ArgumentParser
+import logging
+from typing import NamedTuple, Literal
+import meshio
+from mpi4py import MPI
+import scipy.io
+import datetime
+import dolfinx
+from structlog import get_logger
+from importlib.metadata import metadata
+meta = metadata("cardiac-geometriesx")
+__version__ = meta["Version"]
+
+logger = get_logger()
+
+comm = MPI.COMM_WORLD
+mode = 0 # corresponds to a custom mode in the UKB EDES atlas
+# Load UKB EDES atlas from .mat file
+mat_data = scipy.io.loadmat("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/cardiacDL/refs/Data/BioBank_EDES_200.mat") #Replace with the actual path to your library folder
+pca = mat_data['pca200'][0, 0]
+
+# functions to reconstruct shape given PC scores
+def reconstruct_shape(score, atlas, num_scores = 25):
+    d = score * np.sqrt(atlas["LATENT"][0:num_scores]).T
+    shape = atlas["MU"] + np.matmul(d, atlas["COEFF"][:, :num_scores].T)
+    return shape.T
+
+# Extract ED phase as (N, 3) mesh
+def get_ED_mesh_from_shape(shape):
+    N = len(shape)
+    return shape[:N // 2].reshape(-1, 3)
+
+def get_ES_mesh_from_shape(shape):
+    N = len(shape)
+    return shape[N // 2:].reshape(-1, 3)
+
+class Points(NamedTuple):
+    ED: np.ndarray
+    ES: np.ndarray
+
+# import necessary ET Indices File- this is the same as the connectivity.txt file in the UKB atlas
+et_indices = np.loadtxt("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/biv-me/src/model/ETIndicesSorted.txt").astype(int) - 1 #matlab indexing to python
+print(et_indices)
+# Import virtual cohort sheet
+virtual_cohort = pd.read_excel("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/cardiacDL/refs/Data/virtual_cohort_data.xlsx")
+pc_columns = [col for col in virtual_cohort.columns if col.startswith('PC')]
+pc_scores = virtual_cohort[pc_columns]
+
+patient = pc_scores.loc[1].to_numpy()  # Example: get the first patient's PC scores
+patient_shape = reconstruct_shape(patient, pca)
+patient_ed = get_ED_mesh_from_shape(patient_shape)
+patient_es = get_ES_mesh_from_shape(patient_shape)
+
+import sys
+sys.path.append("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/ukb-atlas/src")
+
+import ukb
+from ukb import atlas, surface, mesh, clip
+out_dir = f"/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/output/data-full/mode_{mode}"
+outdir = Path("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/output/data-full")  / f"mode_{mode}"
+outdir.mkdir(parents=True, exist_ok=True)
+# custom_surface = ukb.surface.Surface()
+unwanted_nodes = (5630, 5655, 5696, 5729)
+points = Points(
+    ED=np.delete(patient_ed, unwanted_nodes, axis=0),
+    ES=np.delete(patient_es, unwanted_nodes, axis=0),
+)
+
+ukb.surface.main(folder=outdir, custom_points=points) # generates the various surfaces
+
+ukb.mesh.main(
+    folder=out_dir,
+    case="ED",
+    char_length_max=5.0,
+    char_length_min=5.0,
+    clipped=False,
+)
+
+sys.path.append("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/cardiac-geometriesx/src")
+import cardiac_geometries as cgx
+
+geometry = cgx.utils.gmsh2dolfin(comm=comm, msh_file = out_dir + "/ED.msh")
+std = 1.5
+case = "ED"
+char_length_max = 5.0
+char_length_min = 5.0
+fiber_angle_endo = 60.0
+fiber_angle_epi = -60.0
+fiber_space = "DG_0"
+clipped = False
+create_fibers = True
+
+
+if comm.rank == 0:
+    (outdir / "markers.json").write_text(
+        json.dumps(geometry.markers, default=cgx.utils.json_serial)
+    )
+    (outdir / "info.json").write_text(
+        json.dumps(
+            {
+                "mode": mode,
+                "std": std,
+                "case": case,
+                "char_length_max": char_length_max,
+                "char_length_min": char_length_min,
+                "fiber_angle_endo": fiber_angle_endo,
+                "fiber_angle_epi": fiber_angle_epi,
+                "fiber_space": fiber_space,
+                "cardiac_geometry_version": __version__,
+                "mesh_type": "ukb",
+                "clipped": clipped,
+                "timestamp": datetime.datetime.now().isoformat(),
+            }
+        )
+    )
+
+if create_fibers:
+    try:
+        import ldrb
+    except ImportError as ex:
+        msg = (
+            "To create fibers you need to install the ldrb package "
+            "which you can install with pip install fenicsx-ldrb"
+        )
+        raise ImportError(msg) from ex
+
+    markers = cgx.mesh.transform_markers(geometry.markers, clipped=clipped)
+    system = ldrb.dolfinx_ldrb(
+        mesh=geometry.mesh,
+        ffun=geometry.ffun,
+        markers=markers,
+        alpha_endo_lv=fiber_angle_endo,
+        alpha_epi_lv=fiber_angle_epi,
+        beta_endo_lv=0,
+        beta_epi_lv=-0,
+        fiber_space=fiber_space,
+    )
+
+    cgx.fibers.utils.save_microstructure(
+        mesh=geometry.mesh,
+        functions=(system.f0, system.s0, system.n0),
+        outdir=outdir,
+    )
+
+    for k, v in system._asdict().items():
+        if v is None:
+            continue
+        if fiber_space.startswith("Q"):
+            # Cannot visualize Quadrature spaces yet
+            continue
+
+        logger.debug(f"Write {k}: {v}")
+        with dolfinx.io.VTXWriter(comm, outdir / f"{k}-viz.bp", [v], engine="BP4") as vtx:
+            vtx.write(0.0)
+
+geo = cgx.geometry.Geometry.from_folder(comm=comm, folder=outdir)
+
+sys.path.append("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/sscp25-deep-learning-cardiac-mechanics")
+
+results_dir = Path("/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/output/results")
+results_dir.mkdir(parents=True, exist_ok=True)
+import run_simulation_full
+results_dir = "/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/output/results-full/"
+run_simulation_full.main(mode = mode, datadir="/mnt/c/Users/rkona/Documents/SSCP25/Cardiac_Deep_Learning/output/data-full/", resultsdir =results_dir)
