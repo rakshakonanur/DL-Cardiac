1bc9c391a200be3dc1ac56d59f80cd62c93e2f72
Raksha Konanur <rakshakonanur@Rakshas-MacBook-Pro.local>
2025-08-05 21:22:35 -0700

    updating command line settings

--- src/simulation.py
+++ src/simulation.py
@@ -5,7 +5,11 @@ class Simulation:
     def __init__(self, mode: int = -1, case = "unloaded_ED",
                  results_dir: str = "output/results-full", 
                  data_dir: str = "output/data-full/", 
-                 solver_path: str = "../clones/rk-sscp25-deep-learning-cardiac-mechanics"):
+                 solver_path: str = "../clones/rk-sscp25-deep-learning-cardiac-mechanics",
+                 PLV: float = 15.0,
+                 PRV: float = 3.0,
+                 Ta: float = 120.0,
+                 N: int = 200):
 
         self.mode = mode
         if case == "both":
@@ -14,7 +18,12 @@ class Simulation:
             cases = ["ED","ES","unloaded_ED"]
         else:
             cases = [case]
+
         self.cases = cases
+        self.PLV = PLV
+        self.PRV = PRV
+        self.Ta = Ta
+        self.N = N
         self.results_dir = results_dir
         self.data_dir = data_dir
         self.results_dir.mkdir(parents=True, exist_ok=True)
@@ -24,12 +33,18 @@ class Simulation:
         for case in self.cases:
             sys.path.append(self.solver_path)
             import run_simulation_full
-            run_simulation_full.main(mode = self.mode, case = case, datadir=self.data_dir, resultsdir = self.results_dir)
+            run_simulation_full.main(mode = self.mode, case = case, datadir=self.data_dir, resultsdir = self.results_dir,
+                                     PLV = self.PLV, PRV = self.PRV, TA = self.Ta, N = self.N)
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Run cardiac mechanics simulation.")
+    parser.add_argument("--case", type=str, default="unloaded_ED", help="Simulation case")
+    parser.add_argument("--PLV", type=float, default=15.0, help="Left ventricular pressure")
+    parser.add_argument("--PRV", type=float, default=3.0, help="Right ventricular pressure")
+    parser.add_argument("--Ta", type=float, default=120.0, help="Active stress time constant")
+    parser.add_argument("--N", type=int, default=200, help="Number of time steps for simulation")
     parser.add_argument("--mode", type=int, default=-1, help="Simulation mode")
     parser.add_argument("--results_dir", type=str, default="output/results-full", help="Directory for results")
     parser.add_argument("--data_dir", type=str, default="output/data-full/", help="Directory for data")
@@ -38,5 +53,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     sim = Simulation(mode=args.mode, results_dir=Path(args.results_dir), 
-                     data_dir=Path(args.data_dir), solver_path=args.solver_path)
+                     data_dir=Path(args.data_dir), solver_path=args.solver_path,
+                     PLV=args.PLV, PRV=args.PRV, Ta=args.Ta, N=args.N)
     sim.run()
