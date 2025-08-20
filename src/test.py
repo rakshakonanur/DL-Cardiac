20841146202914f83687f171ed44a7dfed743bfa
Raksha Konanur <rakshakonanur@wifi-10-41-99-112.wifi.berkeley.edu>
2025-08-20 16:26:21 -0700

    minor changes

--- src/test.py
+++ src/test.py
@@ -8,8 +8,8 @@ import scipy.io
 
 mat_data = scipy.io.loadmat("../refs/BioBank_EDES_200.mat")
 pca = mat_data['pca200'][0, 0]
-score =[-0.34616572, -0.25487567, -0.39251532, -0.11185141, -0.30102752, -0.47441795,
- -0.06028185,  0.46325936,  0.2368012,   0.66624435]
+score =[0.25885691, 0.01645, -0.32231254, 0.20249553, 0.12602418, 0.14317364,
+ -0.13063409, 0.54752804, 0.20553535, -0.005437]
 
 # Predicted values: [-0.14505391, -0.12284824, -0.27698756,  0.0537183,  -0.09379592, -0.29158689,
 #  -0.13056078,  0.42569234,  0.07263599,  0.75241125]
@@ -44,5 +44,5 @@ points = shape.Points(
     unloaded_ED=np.delete(patient_ed, unwanted_nodes, axis=0),
 )
 
-ukb.surface.main(case="both", folder=Path(f"./true"), custom_points=points)
+ukb.surface.main(case="both", folder=Path(f"./"), custom_points=points)
 
